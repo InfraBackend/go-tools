c0b268aabdbea7c8ecdc719a2666fa343aa3ecbd
huzhonglan <huzhonglan@mavericksmobile.com>
2022-09-08 16:53:18 +0800

    添加jwt

---
+++ middleware/JWTAuthMiddleware.go
@@ -0,0 +1,41 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+	"utils/jwt"
+)
+
+func JWTAuthMiddleware() func(ctx *gin.Context) {
+	return func(ctx *gin.Context) {
+		// 根据实际情况取TOKEN, 这里从request header取
+		tokenStr := ctx.Request.Header.Get("Authorization")
+		if tokenStr == "" {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"code": 1,
+				"msg":  "ERR_AUTH_NULL",
+			})
+			return
+		}
+
+		claims, err := jwt.ParseToken(tokenStr)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"code": 2,
+				"msg":  "ERR_AUTH_INVALID",
+			})
+			return
+		} else if time.Now().Unix() > claims.ExpiresAt {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"code": 3,
+				"msg":  "ERR_AUTH_EXPIRED",
+			})
+			return
+		}
+
+		// 此处已经通过了, 可以把Claims中的有效信息拿出来放入上下文使用
+		ctx.Set("username", claims.Username)
+		ctx.Next()
+	}
+}
